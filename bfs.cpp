f82ceefbb02a3aca3f99085fc4b7005247b696bd
Rohitadhav123 <159225705+Rohitadhav123@users.noreply.github.com>
2025-05-03 23:27:43 +0530

    bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,68 +1,60 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-const int MAX=100;
-int visited[MAX];
+const int MAX = 100;
 int adj[MAX][MAX];
-int n,e;
+bool visited[MAX];
+int n, e;
 
-void bfs(int start)
-{
+void bfs(int start) {
     int queue[MAX];
-    int front=0,rear=0;
-    visited[start]=true;
-    queue[rear++]=start;
+    int front = 0, rear = 0;
 
-   while(front < rear)
-   {
-    int node=queue[front++];
-    cout<<node<< " ";
+    visited[start] = true;
+    queue[rear++] = start;
 
-    for(int i=0;i<n;i++)
-    {
-        if(adj[node][i]==1 && !visited[i])
-        {
-        visited[i]=true;
-        queue[rear++]=i;
+    while (front < rear) {
+        int node = queue[front++];
+        cout << node << " ";
+
+        for (int i = 0; i < n; i++) {
+            if (adj[node][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                queue[rear++] = i;
+            }
         }
     }
-   }
-
 }
 
-int main()
-{
+int main() {
+    cout << "Enter number of nodes: ";
+    cin >> n;
+    cout << "Enter number of edges: ";
+    cin >> e;
 
-    cout<<"Enter the number of node :";
-    cin>>n;
-    cout<<"Enter the number of edges :";
-    cin>>e;
+    // Initialize adjacency matrix
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            adj[i][j] = 0;
 
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            adj[i][j]=0;
-        }
-
-    }
-    cout<<"Enter the edges (u,v)";
-    for(int i=0;i<e;i++)
-    {
-        int u,v;
-        cin>>u>>v;
-        adj[u][v]=1;
-        adj[v][u]=1;
+    cout << "Enter edges (u v):\n";
+    for (int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = 1;
+        adj[v][u] = 1; // for undirected graph
     }
 
-    cout<<"Ener start node :";
     int start;
-    cin>>start;
+    cout << "Enter starting node for BFS: ";
+    cin >> start;
 
-    for(int i=0;i<n;i++)
-    {
-        visited[i]=false;
-    }
+    // Initialize visited array
+    for (int i = 0; i < n; i++)
+        visited[i] = false;
+
+    cout << "BFS Traversal: ";
     bfs(start);
 
+    return 0;
 }
